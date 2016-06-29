1b650472663e2fba7b4700e0edb55e1f83d87238
Zader Zheng <yumaoshu@gmail.com>
2016-06-29 11:37:29 +0800

    use gvim to install plugins

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@ current=`pwd`
 ln -s $current/.vimrc.local ~/.vimrc.local
 ln -s $current/.vimrc.bundles.local ~/.vimrc.bundles.local
 
-vim +BundleInstall +BundleClean +qall!
+gvim +BundleInstall +BundleClean +qall!
